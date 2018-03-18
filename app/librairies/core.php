200311cf827e3627a640349b91c78dd443055e56
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-18 21:58:31 +0100

    Suppresion de l'accés au directory création du core.php, et creation de class Core pour permettre à envoyer des paramétre à partir de l'url

--- app/librairies/core.php
+++ app/librairies/core.php
@@ -1,1 +1,20 @@
-Hello
+<?php
+/*
+ * App core class
+ * Crée les URL & charge le core controlleur
+ * la class Core nous permettra d'utiliser ce format D'URL ../Controller/method/parametre
+ */
+class Core {
+    Protected $currentController = 'Home';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct(){
+        $this->getUrl();
+    }
+    //test si les paramétres de l'url fonctionne
+    public function getUrl(){
+        echo $_GET['url'];
+    }
+
+} 
--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
 
     require_once '../app/bootstrap.php';
+
+    //instantier le core library
+    $init = new Core;
