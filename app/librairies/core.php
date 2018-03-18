66a1f176e54c33cd6231a10c032e21e3c66e46a1
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-18 23:42:48 +0100

    Factorisation de l'url et les mettres dans des arrays ordonnées en supprimant tout les caractéres spéciaux et les /

--- app/librairies/core.php
+++ app/librairies/core.php
@@ -5,16 +5,28 @@
  * la class Core nous permettra d'utiliser ce format D'URL ../Controller/method/parametre
  */
 class Core {
-    Protected $currentController = 'Home';
+    //Format d'url : Project3/Controller
+    Protected $currentController = 'Pages';
+    //Format d'url : Project3/Controller/Method
     protected $currentMethod = 'index';
+    //Format d'url : Project3/Controller/Method/param
     protected $params = [];
 
     public function __construct(){
-        $this->getUrl();
+        print_r($this->getUrl());
     }
-    //test si les paramétres de l'url fonctionne
+
+    //Test si un paramétre existe dans l'url et filtrer l'url des espaces blanc,et suppresion des caractéres spéciaux
+    //Et factoriser l'url dans un array sans l'utilisation des '/'
     public function getUrl(){
-        echo $_GET['url'];
+        //echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //Coupe l'url et retourne un tableau ordonnées séparée par '/'
+            $url = explode('/',$url);
+            return $url;
+        }
     }
 
 } 
