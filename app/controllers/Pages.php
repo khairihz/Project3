324b46115fcd68d910847e0757d747243550d156
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-20 16:54:16 +0100

    Mise en place de modéle index et about

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,10 +4,15 @@
         public function __construct(){
         }
         public function index(){
-            $this->view('pages/index', ['title'=>'Jean Forteroche']);
+            $data =['title'=>'Billet simple pour l\'alaska',
+            'description'=>'Vous trouverez dans ce blog toutes les épisodes de ce livre'
+            ];
+            $this->view('pages/index',$data);
         }
         
         public function about(){
-            $this->view('pages/about',['title'=>'À propos de jean forteroche']);
+            $data = ['title'=>'À propos de jean forteroche',
+                    'description'=>'Brief présentation de l\'auteur'];
+            $this->view('pages/about',$data);
         }
     }
