a8252dfa27ca2ccfc94b5b5464acb936c7cf47b3
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-20 14:40:22 +0100

    Préparer les déclarations avec des requêtes/vérifications de type des valeurs/Avoir les résultats sous forme de tableau d'objet

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -33,4 +33,55 @@
             echo $this->error;
         }
     }
+        // Préparer les déclarations avec des requêtes 
+        public function query($sql){
+            $this->stmt = $this->dbh->prepare($sql);
+    
+        }
+    
+        //Vérification de type de valeur : Int,bool,Strg ou null
+        public function bind($param, $value , $type = null){
+            if (is_null($type)) {
+                switch(true){
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+    
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+    
+                    default:
+                        $type = PDO::PARAM_STR;
+    
+                }
+            }
+    
+            $this->stmt->bindValue($param, $value, $type);
+        }
+    
+        // Executé la déclaration préparée
+        public function execute(){
+            return $this->stmt->execute();
+        }
+    
+        //Avoir les résultats sous forme de tableau d'objet
+        public function resultSet(){
+            $this->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+    
+        // récuperer l'enregistrement unique en tant qu'objet
+        public function single(){
+            $this->execute();
+            return $this->stmt->fetch(PDO::FETCH_OBJ);
+        }
+        // Obtenir le nombre de lignes
+        public function rowCount(){
+            return $this->stmt->rowCount();
+        }
  }
