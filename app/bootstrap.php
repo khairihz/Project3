51f03d3235e33e4a559708906c8f2999ad5da35f
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-24 00:28:48 +0100

    Redirect into session helpers to load the flash message:Bienvenue Jean  when login

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,7 @@
     // charger les helpers
 
     require_once 'helpers/url_helpers.php';
+    require_once 'helpers/session_helper.php';
 
     //require_once 'libraries/Core.php';
     //require_once 'libraries/Controller.php';
