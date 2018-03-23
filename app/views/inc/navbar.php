59b301da2990653f0ee82e84630705af402bca3a
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-23 14:12:01 +0100

    Modification of the navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -17,9 +17,6 @@
             <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT;?>/user/login">Administrateur</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="<?php echo URLROOT;?>/users/Contact">Contact</a>
-            </li>
           </ul>
         </div>
       </div>
