2e9f806dddf4f6518d4998acf7d48c9393cd5fdb
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-26 04:17:42 +0100

    Creating the view of a simple post

--- app/views/admin/add.php
+++ app/views/admin/add.php
@@ -3,6 +3,7 @@
     <div class="container">
         <div class="row">
         <?php require APPROOT .'/views/admin/inc/widget.php';?>
+ 
 <!-- Ajout d'épisode -->
     <div class="col-md-9">
         <div class="panel panel-default">
@@ -14,26 +15,22 @@
         <form action ="<?php echo URLROOT; ?>/admin/add" method = "post">
             <div class="form-group">
                 <label for="title">Titre de l'épisode</label>
-                <input type="text" name ="title" class="form-control <?php echo (!empty($data
-                ['title_err'])) ? 'is-invalid' : '' ; ?>" placeholder="Page Title" value="<?php $data['title'];?>">
-                <span class="invalid-feedback">
-                <?php echo $data['title_err']; ?>
-                </span>
+                <input type="text" name ="title" placeholder="Titre d'épisode" class="form-control <?php echo (!empty(@$data
+                ['title_err'])) ? 'is-invalid' : '' ; ?>" value="<?php $data['title'];?>">
+                <span class="invalid-feedback"><?php echo @$data['title_err']; ?></span>
             </div>
 
             <div class="form-group">
-                <label for= "content">Contenu</label>
-                <textarea name="content" class="form-control <?php echo (!empty($data
-                ['content_err'])) ? 'is-invalid' : '' ; ?>" placeholder="Page Body">
-                    <?php echo $data['content'];?>
+                <label for= "content">Contenu :</label>
+                <textarea name="content" placeholder="Veuillez écrire votre contenu"
+                class="form-control <?php echo (!empty(@$data
+                ['content_err'])) ? 'is-invalid' : '' ; ?>" <?php echo $data['content'];?>>
                 </textarea>
-                <span class="invalid-feedback">
-                <?php echo $data['content_err']; ?>
-                </span>
+                <span class="invalid-feedback"><?php echo @$data['content_err']; ?></span>
             </div>
 
             <div class="form-group">
-                <input type="submit" class="btn btn-default" value="Valider">
+                <input name= "submit" type="submit" class="btn btn-default" value="Valider">
             </div>
         </form>
         </div>
---
+++ app/views/posts/show.php
@@ -0,0 +1,27 @@
+<?php  require APPROOT .'/views/inc/header.php'; ?>
+<header class="masthead" style="background-image: url('<?php echo URLROOT; ?>/bootstrap/img/post-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1><?php echo $data['post']->title;?></h1>
+              <span class="meta">Publié le
+                <?php echo $data['post']->c_date; ?></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <p><?php echo $data['post']->content ?></p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <?php  require APPROOT .'/views/inc/footer.php'; ?>
