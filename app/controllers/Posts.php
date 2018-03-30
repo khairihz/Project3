d5aae09384aac66b3623c1f568bd955526aeb58f
kheireddine-hezzi <khairi.hezzi@gmail.com>
2018-03-30 20:28:50 +0100

    Creating plugin comment system

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -22,7 +22,8 @@
                 'post'=> $post,
                 'comments'=>$comments
             ];
-
             $this->view('posts/show', $data);
+
+            }
         }
-    }
+    
--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -36,7 +36,10 @@
       <p><strong><?php echo $comments->author ;?></strong> le <?php echo $comments->comment_date_fr; ?></p>
     </div>
     <div class ="form-group floating-label-form-group controls">
-      <p><?php echo $comments->comment;?><button type="submit" class="btn btn-danger pull-right  report" id="sendMessageButton">Signaler</button></p>
+      <p><?php echo $comments->comment;?></p>
+      <div class="form-group">
+              <button type="submit" class="btn btn-danger pull-right mb-3 !important" id="sendMessageButton">Signaler</button>
+            </div>
 
     </div>
   <?php endforeach;?>
